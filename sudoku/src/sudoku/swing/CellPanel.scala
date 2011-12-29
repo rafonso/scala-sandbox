1492dcd8302e9b6a6e03c54294c09d1dd4dbe6da
rafonso <rucafonso@hotmail.com>
2011-12-28 22:39:28 -0200

    Simplificação no tratamento de MouseClicked. Tentativa de tratamento de MouseWheelMoved e KeyPressed.

--- sudoku/src/sudoku/swing/CellPanel.scala
+++ sudoku/src/sudoku/swing/CellPanel.scala
@@ -3,16 +3,19 @@ package sudoku.swing
 import java.awt.Color
 import java.awt.Font
 import scala.collection.mutable.Subscriber
-import scala.swing.Dialog.Message
-import scala.swing.event.MouseClicked
-import scala.swing.Dialog
 import scala.swing.FlowPanel
 import scala.swing.Label
 import scala.swing.Swing
+import scala.swing.Dialog._
 import sudoku.Cell
-import sudoku.CellEvent
 import sudoku.CellEvaluated
+import sudoku.CellEvent
 import sudoku.CellValueChanged
+import scala.swing.event.MouseClicked
+import scala.swing.Dialog
+import scala.swing.event.KeyPressed
+import scala.swing.event.MouseWheelMoved
+import scala.swing.event.Key._
 
 class CellPanel(row: Int, col: Int) extends FlowPanel with Subscriber[CellEvent, Cell] {
 
@@ -27,6 +30,14 @@ class CellPanel(row: Int, col: Int) extends FlowPanel with Subscriber[CellEvent,
   private val FundoNormal = this.background
   private val FundoAvaliado = Color.BLUE
 
+  private def getValue = Dialog.showInput(parent = this,
+    "Choice a Value for Cell " + this.row + ", " + this.col,
+    "Sudoku",
+    Message.Question,
+    Swing.EmptyIcon,
+    Empty :: (1 to 9).toList,
+    0)
+
   this.contents += this.label
 
   this.border = Swing.MatteBorder(
@@ -36,30 +47,25 @@ class CellPanel(row: Int, col: Int) extends FlowPanel with Subscriber[CellEvent,
     if ((col + 1) % 3 == 0) 2 else 1, // Borda direita
     Color.BLACK)
 
-  super.listenTo(this.mouse.clicks)
+  super.listenTo(this.mouse.clicks, this.keys, this.mouse.wheel)
 
   this.reactions += {
-    case MouseClicked(_, _, _, clicks, _) => {
-      if (clicks == 2) {
-        val value = Dialog.showInput(parent = this,
-          "Choice a Value for Cell " + this.row + ", " + this.col,
-          "Sudoku",
-          Message.Question,
-          Swing.EmptyIcon,
-          Empty :: (1 to 9).toList,
-          0)
-        value match {
-          case Some(v) => {
-            this.label.text = v.toString()
-            this.label.font = FonteOriginal
-          }
-          case _ => {
-            this.label.text = Empty
-            this.label.font = FonteNormal
-          }
+    case MouseClicked(source, _, modifiers, 2, _) if ((source == this)) => {
+      println(modifiers)
+      getValue match {
+        case Some(value) => {
+          this.label.text = value.toString()
+          this.label.font = FonteOriginal
+        }
+        case _ => {
+          this.label.text = Empty
+          this.label.font = FonteNormal
         }
       }
     }
+//    case MouseWheelMoved(source, point, modifiers, rotation) if (source == this ) => println(rotation + " - -" + modifiers)
+//    case KeyPressed(source, key, _, _) if (source == this) => println(key)
+//
   }
 
   def value: Option[Int] = if (this.label.text == Empty) None else Some(this.label.text.toInt)
