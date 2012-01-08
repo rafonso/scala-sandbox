eeda66e055ce1f2e9827dabee75658da4d7edf24
rafonso <rucafonso@hotmail.com>
2012-01-08 17:11:12 -0200

    Exibição das Guess Cells.

--- sudoku/src/sudoku/swing/SudokuApp.scala
+++ sudoku/src/sudoku/swing/SudokuApp.scala
@@ -3,44 +3,87 @@
  */
 package sudoku.swing
 
+import java.awt.Insets
+
 import scala.collection.mutable.Subscriber
 import scala.swing.BorderPanel.Position
 import scala.swing.Dialog.Message
-import scala.swing.GridBagPanel._
+import scala.swing.GridBagPanel.Anchor
+import scala.swing.GridBagPanel.Fill
 import scala.swing.event.ButtonClicked
 import scala.swing.event.Key
+import scala.swing.Dimension
+import scala.swing.Alignment
 import scala.swing.BorderPanel
+import scala.swing.BoxPanel
 import scala.swing.Button
 import scala.swing.Dialog
-import scala.swing.Dimension
-import scala.swing.FlowPanel
 import scala.swing.GridBagPanel
 import scala.swing.GridPanel
 import scala.swing.Label
 import scala.swing.MainFrame
+import scala.swing.Orientation
+import scala.swing.ScrollPane
 import scala.swing.SimpleSwingApplication
+import scala.swing.Table
+
+import javax.swing.table.AbstractTableModel
 import javax.swing.UIManager
-import java.awt.Insets
-import sudoku.SudokuPuzzle._
+import sudoku.Cell
 import sudoku.CellStatus
+import sudoku.ChangeAlghoritimEvent
+import sudoku.GuessValueFailedEvent
+import sudoku.GuessValueTryingEvent
 import sudoku.RunningEvent
 import sudoku.RunningState
 import sudoku.SudokuEvent
 import sudoku.SudokuPuzzle
-import sudoku.SudokuSolver
 import sudoku.SudokuPuzzleIteractionEvent
 import sudoku.SudokuType
-import sudoku.ChangeAlghoritimEvent
-import scala.swing.Swing
-import scala.swing.Alignment
 
 /**
  * @author rafael
  *
  */
 object SudokuApp extends SimpleSwingApplication with Subscriber[SudokuEvent, SudokuType] {
+
   // Uses Native L&F
   UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+
+  class GuessTableModel extends AbstractTableModel {
+
+    private var guessesCells: Seq[Cell] = Seq.empty
+
+    def getRowCount() = guessesCells.size
+
+    def getColumnCount() = 3
+
+    override def getColumnName(col: Int) = col match {
+      case 0 => "Row"
+      case 1 => "Column"
+      case 2 => "Value"
+      case _ => Empty
+    }
+
+    def getValueAt(row: Int, col: Int) = {
+      if (row < this.getRowCount()) {
+        val selectedCell = guessesCells(row)
+
+        col match {
+          case 0 => selectedCell.row.toString
+          case 1 => selectedCell.col.toString
+          case 2 => selectedCell.value.getOrElse(Empty).toString
+          case _ => Empty
+        }
+      } else Empty
+    }
+
+    def updateTable(cells: Seq[Cell]) {
+      this.guessesCells = cells.reverse
+      fireTableDataChanged()
+    }
+
+  }
 
   val RunTitle = "Run"
   val PuzzleTitle = "Puzzle"
@@ -59,6 +102,10 @@ object SudokuApp extends SimpleSwingApplication with Subscriber[SudokuEvent, Sud
     enabled = false
     mnemonic = Key.C
   }
+  val tblGuesses = new Table {
+    model = new GuessTableModel
+    preferredViewportSize = new Dimension(120, 100)
+  }
   val lblIteractions = new Label
   val lblTime = new Label
   val lblAlghoritim = new Label
@@ -75,7 +122,6 @@ object SudokuApp extends SimpleSwingApplication with Subscriber[SudokuEvent, Sud
       case ButtonClicked(`btnAction`) if (!this.board.isEmpty) => this.run
       case ButtonClicked(`btnPuzzle`)                          => this.openPuzzleDialog
       case ButtonClicked(`btnClean`)                           => this.cleanBoard
-      //      case PuzzleDialogClosed(None)       => this.cleanBoard
       case PuzzleDialogClosed(Some(text))                      => this.fillBoard(text.trim, (btnPuzzle.text == NewPuzzleTitle))
     }
   }
@@ -103,7 +149,10 @@ object SudokuApp extends SimpleSwingApplication with Subscriber[SudokuEvent, Sud
     this.btnClean.enabled = false
     this.lblIteractions.text = ""
     this.lblTime.text = ""
+    this.refreshTableGuess(Seq.empty)
   }
+
+  def refreshTableGuess(guesses: Seq[Cell]) = tblGuesses.model.asInstanceOf[GuessTableModel].updateTable(board.puzzle.guessesCells)
 
   private def fillBoard(text: String, reNewPuzzle: Boolean) {
     if (reNewPuzzle) {
@@ -188,6 +237,9 @@ object SudokuApp extends SimpleSwingApplication with Subscriber[SudokuEvent, Sud
         contents += (SudokuApp.this.btnAction, SudokuApp.this.btnPuzzle, SudokuApp.this.btnClean)
         hGap = 2
       }, Position.South)
+      add(new BoxPanel(Orientation.Vertical) {
+        contents += (new Label("Guesses Values"), new ScrollPane(SudokuApp.this.tblGuesses))
+      }, Position.East)
     }
     preferredSize = new Dimension(300, 400)
     resizable = true
@@ -214,6 +266,8 @@ object SudokuApp extends SimpleSwingApplication with Subscriber[SudokuEvent, Sud
         lblTime.text = "%,10d".format((System.currentTimeMillis() - t0))
       }
       case (_, ChangeAlghoritimEvent(description)) => lblAlghoritim.text = description
+      case (_, GuessValueTryingEvent(guessCell))   => this.refreshTableGuess(board.puzzle.guessesCells)
+      case (_, GuessValueFailedEvent(guessCell))   => this.refreshTableGuess(board.puzzle.guessesCells)
       case (_, _)                                  =>
     }
   }
